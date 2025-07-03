5f44d7e1b8596c7404a9511aad259e2daf9181e0
SA JAEHEON <jls15900@gmail.com>
2025-07-03 16:29:13 +0900

    feat: 자동 로그인 기능 구현. 이전에 로그인 한 기기라면, 로그인 화면 건너뛰고 바로 홈 스크린으로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:lunary/screens/account/login_screen.dart';
-// import 'package:lunary/screens/splash/splash_screen.dart';
+import 'package:lunary/screens/home/home_screen.dart';
 
 void main() async {
   // 환경변수 불러오기
@@ -23,7 +24,19 @@ class LunaryApp extends StatelessWidget {
     return MaterialApp(
       title: 'Lunary',
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // 로그인된 사용자가 있으면 홈으로, 없으면 로그인 화면으로
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasData) {
+            return HomeScreen();
+          }
+          return LoginScreen();
+        },
+      ),
     );
   }
 }
